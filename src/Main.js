4aac485d7efa6152920784a675a1de313b3a05b2
b0ft <aldysyach17@gmail.com>
2020-08-31 15:57:37 +0700

    second commit

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+    return(
+        <main className="main">
+            <h1>Title</h1>
+            <div>Products</div>
+        </main>
+    )
+}
+
+export default Main
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+    return(
+        <nav className="nav">
+            <div className="mini-nav">
+                Left
+            </div>
+            <div className="mini-nav">
+                Right
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
